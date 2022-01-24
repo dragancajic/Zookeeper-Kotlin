118d4d5baa951495d9cc2868571c7aaa2d81b086
Dragan Cajic <cajic_dragan@yahoo.com>
2022-01-24 10:02:56 +0100

    Stage 2/4: Show me an animal!

--- Zookeeper/task/src/Zookeeper.kt
+++ Zookeeper/task/src/Zookeeper.kt
@@ -1,8 +1,25 @@
+var camel = """Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+
 fun main() {
     // write your code here
-    println("I love animals!")
-    println("Let's check on the animals...")
-    println("The deer looks fine.")
-    println("The bat looks happy.")
-    println("The lion looks healthy.")
+    print(camel.trimIndent())
 }
